f4632bd048e3d92883a78ded8835ee9d5946556f
Edgar-ae <edgar.aggre@gmail.com>
2023-04-05 12:13:31 -0500

    identification finished

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,7 @@ from . import views # corto y pego de ./mysite/urls.py
 
 urlpatterns = [
     path('', views.index), # corto y pego de ./mysite/urls.py
-    path('form_new_fut/', views.form_new_fut), #corto y pego de ./mysite/urls.py
-    path('my_fut', views.my_fut)
+    path('form_new_fut/identification', views.form_new_fut), #corto y pego de ./mysite/urls.py
+    path('my_fut', views.my_fut),
+    path('form_new_fut/process', views.create_fut_details)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -6,8 +6,13 @@ from django.http import HttpResponse # agregamos el HttpResponse
 def index(request):
     return render(request,'index.html')
 
-def form_new_fut(request):
-    return render(request, 'create_fut/form_new_fut.html')
-
 def my_fut(request):
     return render(request, 'view_fut/fut.html')
+
+# Create FUT
+
+def form_new_fut(request):
+    return render(request, 'create_fut/identification.html')
+
+def create_fut_details(request):
+    return render(request, 'create_fut/process.html')
