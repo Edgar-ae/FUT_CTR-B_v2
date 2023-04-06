01309fe616eccb4da5314f05784d7432f57287ce
Edgar-ae <edgar.aggre@gmail.com>
2023-04-05 22:59:43 -0500

    first function with cookies finished

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index), # corto y pego de ./mysite/urls.py
     path('form_new_fut/identification', views.form_new_fut), #corto y pego de ./mysite/urls.py
     path('my_fut', views.my_fut),
-    path('form_new_fut/process', views.create_fut_details)
+    path('form_new_fut/processtd', views.create_fut_details, name="lol")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse # agregamos el HttpResponse
 
 # Create your views here.
@@ -11,8 +11,42 @@ def my_fut(request):
 
 # Create FUT
 
+    
+
 def form_new_fut(request):
-    return render(request, 'create_fut/identification.html')
+    if request.method=='GET':
+        return render(request, 'create_fut/identification.html')
+    if request.method=='POST':
+
+
+        mycicle = request.POST.get('cicle')
+
+        response = redirect('lol')
+
+        response.set_cookie('ciclon', mycicle)
+        return response
+
 
 def create_fut_details(request):
-    return render(request, 'create_fut/process.html')
+    if request.method=='GET':
+        print('MMMMMMMMMMM')
+        name = request.COOKIES['ciclon'] #obtenemos el cookie
+        print(name)
+        print('MMMMMMMMMMM')
+        return render(request, 'create_fut/process.html')
+    if request.method=='POST':
+        mycicle = request.POST.get('cicle')
+        print('MMMMMMMMMMM')
+        print('------'+mycicle)
+        print(request.pepo)
+        print('MMMMMMMMMMM')
+        context = {
+        }
+
+        response = render(request, 'create_fut/process.html', context)
+        # setting the cookies
+        response.set_cookie('ciclon', mycicle)
+
+        #response.redirect('lol')
+
+        return response
