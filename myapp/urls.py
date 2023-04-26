abc9708fe7ea1c0ad34b0a951567519af8215078
Edgar-ae <edgar.aggre@gmail.com>
2023-04-26 13:23:58 -0500

    we perfected the method to save the fut in the database

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
     path('', views.index), # corto y pego de ./mysite/urls.py
     path('form_new_fut/identification', views.form_new_fut), #corto y pego de ./mysite/urls.py
     path('my_fut', views.my_fut),
-    path('form_new_fut/processtd', views.create_fut_process, name="lol"),
-    path('form_new_fut/pay', views.create_fut_pay, name="pay"),
-    path('form_new_fut/successful', views.finisher, name="end"),
+    path('form_new_fut/processtd', views.create_fut_process, name="n_process"),
+    path('form_new_fut/pay', views.create_fut_pay, name="n_pay"),
+    path('form_new_fut/finisher', views.finisher, name="n_end"),
+    path('form_new_fut/successful', views.successful, name="n_successful"),
 
     path('my_fut/subir_pdf', views.subir_pdf, name="subir_pdf")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -16,12 +16,7 @@ def index(request):
 
 def my_fut(request):
 
-    # Genramos el número de Expediente
-    Expediente = random.sample(range(0, 9),5)
-    Expediente_cadena = ''.join(map(str, Expediente))
-    # Generamos la contraseña
-    Contraseña = random.sample(range(0, 9),4)
-    Contraseña_cadena = ''.join(map(str, Contraseña))
+    print('Hay que crear el QR!')
     
     return render(request, 'view_fut/fut.html')
 # Create FUT
@@ -29,67 +24,77 @@ def my_fut(request):
     
 
 def form_new_fut(request):
-    if request.method=='GET':
-        return render(request, 'create_fut/identification.html')
-    if request.method=='POST':
+    return render(request, 'create_fut/identification.html')
 
+
+def create_fut_process(request):
+    if request.method=='POST':
         name = request.POST.get('name')
         program = request.POST.get('program')
         dni = request.POST.get('dni')
         phone = request.POST.get('phone')
         cycle = request.POST.get('cycle')
 
-        response = redirect('lol', {
+        return render(request, 'create_fut/process.html', {
             'Name': name,
-            'Program': program
+            'Program': program,
+            'Dni': dni,
+            'Phone': phone,
+            'Cycle': cycle
         })
+    else:
+        return HttpResponse("<h1>404 Not Found :(</h1>")
 
-        # response.set_cookie('c_name', name)
-        # response.set_cookie('c_program', program)
-        # response.set_cookie('c_dni', dni)
-        # response.set_cookie('c_phone', phone)
-        # response.set_cookie('c_cycle', cycle)
-
-        return response
-
-
-def create_fut_process(request):
-    if request.method=='GET':
-        return render(request, 'create_fut/process.html')
+    
+@csrf_exempt  
+def create_fut_pay(request):
     if request.method=='POST':
+        # identification
+        name = request.POST.get('name')
+        program = request.POST.get('program')
+        dni = request.POST.get('dni')
+        phone = request.POST.get('phone')
+        cycle = request.POST.get('cycle')
+        # process
         myrequest = request.POST.get('myrequest')
         order = request.POST.get('order')
         reason = request.POST.get('reason')
-
         # Procedimiento con el PDF
         pdf_file = request.FILES['pdf_file']
         pdf_binary = pdf_file.read()
         pdf_binary_encoded = base64.b64encode(pdf_binary)
 
-        response = redirect('pay')
+        return render(request, 'create_fut/pay.html', {
+            'Name': name,
+            'Program': program,
+            'Dni': dni,
+            'Phone': phone,
+            'Cycle': cycle,
+            'Myrequest': myrequest,
+            'Order': order,
+            'Reason': reason,
+            'Pdf_binary_encoded': pdf_binary_encoded
+        })
+    else:
+        return HttpResponse("<h1>404 Not Found :(</h1>")
 
-        response.set_cookie('c_myrequest', myrequest)
-        response.set_cookie('c_order', order)
-        response.set_cookie('c_reason', reason)
-        response.set_cookie('c_pdf', pdf_binary_encoded)
-
-        return response
-    
 @csrf_exempt  
-def create_fut_pay(request):
-    if request.method=='GET':
-        return render(request, 'create_fut/pay.html')
-    if request.method=='POST':
-        v_name = request.COOKIES['c_name'] #obtenemos el cookie
-        v_program = request.COOKIES['c_program']
-        v_dni = request.COOKIES['c_dni']
-        v_phone = request.COOKIES['c_phone']
-        v_cycle = request.COOKIES['c_cycle']
-        v_myrequest = request.COOKIES['c_myrequest']
-        v_order = request.COOKIES['c_order']
-        v_reason = request.COOKIES['c_reason']
-        v_now_date = date.today()
-        v_pdf_binary = request.COOKIES['c_pdf']
+def finisher(request):
+    if request.method == 'POST':
+         # identification
+        name = request.POST.get('name')
+        program = request.POST.get('program')
+        dni = request.POST.get('dni')
+        phone = request.POST.get('phone')
+        cycle = request.POST.get('cycle')
+        # process
+        myrequest = request.POST.get('myrequest')
+        order = request.POST.get('order')
+        reason = request.POST.get('reason')
+        pdf_memoryview = request.POST.get('pdf_binary_encoded')
+        pdf_bytes = pdf_memoryview.encode("utf-8")
+
+        now_date = date.today()
         
         # Genramos el número de Expediente
         Expediente = random.sample(range(0, 9),5)
@@ -98,24 +103,29 @@ def create_fut_pay(request):
         Contraseña = random.sample(range(0, 9),4)
         Contraseña_cadena = ''.join(map(str, Contraseña))
 
-        my_objet = fut(name=v_name, program=v_program, dni=v_dni, phone=v_phone, cycle=v_cycle, myrequest=v_myrequest, order=v_order, reason=v_reason, date=v_now_date, binary_content=v_pdf_binary, proceeding=Expediente_cadena, password=Contraseña_cadena)
-
+        my_objet = fut(name=name, program=program, dni=dni, phone=phone, cycle=cycle, myrequest=myrequest, order=order, reason=reason, date=now_date, binary_content=pdf_bytes, proceeding=Expediente_cadena, password=Contraseña_cadena)
         my_objet.save()
-        response = redirect('end')
 
+        new_id = my_objet.id
+
+        response = redirect('n_successful')
+        response.set_cookie('New_id', new_id)
         return response
-    
-def finisher(request):
-    if request.method == 'POST':
-        reason = request.POST.get('reason')
-        print('______POST________'+reason)
-        return render(request, 'create_fut/successful.html')
     else:
-        objetos = fut.objects.filter(id=1).values('name').first()['name']
-        return render(request, 'create_fut/successful.html', {
-            'var': objetos
-        })
+        return HttpResponse("<h1>404 Not Found :(</h1>")
     
+    
+def successful(request):
+    my_id = request.COOKIES.get('New_id')
+    objetos = fut.objects.filter(id=my_id).values('name', 'dni', 'order').first()
+    return render(request, 'create_fut/successful.html', {
+        'Name': objetos['name'],
+        'Dni': objetos['dni'],
+        'Order': objetos['order']
+    })
+
+
+
 @csrf_exempt
 def subir_pdf(request):
     if request.method == 'POST':
